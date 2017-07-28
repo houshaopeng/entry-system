beb96870570505182da7ccef64983bea91994172
huangyi <748115626@qq.com>
2017-07-28 15:06:59 +0800

    正则

--- src/components/storeMsg.vue
+++ src/components/storeMsg.vue
@@ -38,17 +38,17 @@
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="网点名称" prop="networkName">
-							<el-input v-model="ruleForm.networkName" placeholder="请输入网点名称"></el-input>
+							<el-input :maxlength="30" v-model="ruleForm.networkName" placeholder="请输入网点名称"></el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="网点联系人" prop="networkContact">
-							<el-input v-model="ruleForm.networkContact" placeholder="请输入网点联系人"></el-input>
+							<el-input :maxlength="30" v-model="ruleForm.networkContact" placeholder="请输入网点联系人"></el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="联系电话" prop="contactTel">
-							<el-input v-model="ruleForm.contactTel" placeholder="请输入联系电话"></el-input>
+							<el-input :maxlength="11" v-model="ruleForm.contactTel" placeholder="请输入联系电话"></el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="24" :md="16" :lg="12">
@@ -79,12 +79,12 @@
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="业务员姓名" prop="salesmanName">
-							<el-input v-model="ruleForm.salesmanName" placeholder="请输入业务员姓名"></el-input>
+							<el-input :maxlength="30" v-model="ruleForm.salesmanName" placeholder="请输入业务员姓名"></el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="业务员工号" prop="salesmanNumber">
-							<el-input v-model="ruleForm.salesmanNumber" placeholder="请输入业务员工号"></el-input>
+							<el-input :maxlength="9" v-model="ruleForm.salesmanNumber" placeholder="请输入业务员工号"></el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="24" :md="24" :lg="24">
@@ -118,12 +118,12 @@
 				<el-row :gutter="10" v-for="company in companys" :key="company.value">
 					<el-col :xs="18" :sm="18" :md="8" :lg="6">
 						<el-form-item label="名称">
-							<el-input v-model="company.companyName" placeholder="请输入公司名称"></el-input>
+							<el-input :maxlength="30" v-model="company.companyName" placeholder="请输入公司名称"></el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="18" :sm="18" :md="8" :lg="6">
 						<el-form-item label="离店距离" prop="companyDistance">
-							<el-input v-model="company.companyDistance" placeholder="请输入距离">
+							<el-input :maxlength="30" v-model="company.companyDistance" placeholder="请输入距离">
 								<template slot="append">米</template>
 							</el-input>
 						</el-form-item>
@@ -196,7 +196,7 @@
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="法人姓名" prop="legalName">
-							<el-input v-model="ruleForm.legalName" placeholder="请输入法人姓名"></el-input>
+							<el-input :maxlength="30" v-model="ruleForm.legalName" placeholder="请输入法人姓名"></el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
@@ -214,14 +214,14 @@
 				<el-row :gutter="10">
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="近三月平均营业额" prop="threeMoney">
-							<el-input v-model="ruleForm.threeMoney" placeholder="请输入近三月平均营业额">
+							<el-input :maxlength="30" v-model="ruleForm.threeMoney" placeholder="请输入近三月平均营业额">
 								<template slot="append">万元</template>
 							</el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="去年全年营业额" prop="yearMoney">
-							<el-input v-model="ruleForm.yearMoney" placeholder="请输入去年全年营业额">
+							<el-input :maxlength="30" v-model="ruleForm.yearMoney" placeholder="请输入去年全年营业额">
 								<template slot="append">万元</template>
 							</el-input>
 						</el-form-item>
@@ -230,13 +230,13 @@
 				<el-row :gutter="10">
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="主营商品1" prop="productName1">
-							<el-input v-model="ruleForm.productName1" placeholder="请输入商品名称">
+							<el-input :maxlength="30" v-model="ruleForm.productName1" placeholder="请输入商品名称">
 							</el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="均价" prop="productPrice1">
-							<el-input v-model="ruleForm.productPrice1" placeholder="请输入商品价格">
+							<el-input :maxlength="30" v-model="ruleForm.productPrice1" placeholder="请输入商品价格">
 								<template slot="append">元</template>
 							</el-input>
 						</el-form-item>
@@ -245,13 +245,13 @@
 				<el-row :gutter="10">
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="主营商品2" prop="productName2">
-							<el-input v-model="ruleForm.productName2" placeholder="请输入商品名称">
+							<el-input :maxlength="30" v-model="ruleForm.productName2" placeholder="请输入商品名称">
 							</el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="均价" prop="productPrice2">
-							<el-input v-model="ruleForm.productPrice2" placeholder="请输入商品价格">
+							<el-input :maxlength="30" v-model="ruleForm.productPrice2" placeholder="请输入商品价格">
 								<template slot="append">元</template>
 							</el-input>
 						</el-form-item>
@@ -260,13 +260,13 @@
 				<el-row :gutter="10">
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="主营商品3" prop="productName3">
-							<el-input v-model="ruleForm.productName3" placeholder="请输入商品名称">
+							<el-input :maxlength="30" v-model="ruleForm.productName3" placeholder="请输入商品名称">
 							</el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="均价" prop="productPrice3">
-							<el-input v-model="ruleForm.productPrice3" placeholder="请输入商品价格">
+							<el-input :maxlength="30" v-model="ruleForm.productPrice3" placeholder="请输入商品价格">
 								<template slot="append">元</template>
 							</el-input>
 						</el-form-item>
@@ -274,8 +274,8 @@
 				</el-row>
 				<el-row :gutter="10">
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
-						<el-form-item label="店铺日均人流量" prop="name">
-							<el-input v-model="ruleForm.name" placeholder="请输入店铺日均人流量">
+						<el-form-item label="店铺日均人流量" prop="dailyPeople">
+							<el-input v-model="ruleForm.dailyPeople" placeholder="请输入店铺日均人流量">
 							</el-input>
 						</el-form-item>
 					</el-col>
@@ -293,8 +293,8 @@
 			<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-position="left" label-width="121px" class="demo-ruleForm">
 				<el-row :gutter="10">
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
-						<el-form-item label="申请人姓名" prop="name">
-							<el-input v-model="ruleForm.name" placeholder="请输入申请人姓名"></el-input>
+						<el-form-item label="申请人姓名" prop="applicantName">
+							<el-input :maxlength="30" v-model="ruleForm.applicantName" placeholder="请输入申请人姓名"></el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
@@ -316,68 +316,109 @@
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
 						<el-form-item label="婚姻状况" prop="maritalStatus">
 							<el-select v-model="ruleForm.maritalStatus" placeholder="请选择婚姻状况">
-								<el-option v-for="item in maritalList" :key="item.value" :label="item.label" :value="item.value">
+								<el-option v-for="item in maritalList" :key="item.value" :label="item.maritalStatus" :value="item.maritalValue">
 								</el-option>
 							</el-select>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
-						<el-form-item label="申请人籍贯" prop="name">
-							<el-input v-model="ruleForm.name" placeholder="请输入申请人籍贯"></el-input>
+						<el-form-item label="申请人籍贯" prop="applicantOrigin">
+							<el-input :maxlength="30" v-model="ruleForm.applicantOrigin" placeholder="请输入申请人籍贯"></el-input>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="24" :md="24" :lg="18">
-						<el-form-item label="申请人户籍地址" prop="name">
+						<el-form-item label="申请人户籍地址" prop="applicantResAddress">
 							<el-col :xs="18" :sm="18" :md="18" :lg="18">
 								<address-picker :opts="obj" v-model="address"></address-picker>
 							</el-col>
 							<el-col :xs="6" :sm="6" :md="6" :lg="6">
-								<el-input v-model="ruleForm.name" placeholder="请输入详细联系地址"></el-input>
+								<el-input v-model="ruleForm.applicantResAddress" placeholder="请输入详细联系地址"></el-input>
 							</el-col>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="24" :md="24" :lg="18">
-						<el-form-item label="申请人现居住地址" prop="name">
+						<el-form-item label="申请人现居住地址" prop="applicantCurrAddress">
 							<el-col :xs="18" :sm="18" :md="18" :lg="18">
 								<address-picker :opts="obj" v-model="address"></address-picker>
 							</el-col>
 							<el-col :xs="6" :sm="6" :md="6" :lg="6">
-								<el-input v-model="ruleForm.name" placeholder="请输入详细联系地址"></el-input>
+								<el-input v-model="ruleForm.applicantCurrAddress" placeholder="请输入详细联系地址"></el-input>
 							</el-col>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="6">
-						<el-form-item label="申请人占股比列" prop="name">
-							<el-input v-model="ruleForm.name" placeholder="请输入申请人占股比列">
+						<el-form-item label="申请人占股比列" prop="applicantPercent">
+							<el-input v-model="ruleForm.applicantPercent" placeholder="请输入申请人占股比列">
 								<template slot="append">%</template>
 							</el-input>
 						</el-form-item>
 					</el-col>
 				</el-row>
-				<el-row :gutter="10" v-for="company in companys" :key="company.value">
+				<el-row :gutter="10">
 					<el-col :xs="24" :sm="24" :md="24" :lg="18">
-						<el-form-item label="联系人信息" prop="name">
-							<el-col :xs="2" :sm="2" :md="2" :lg="1">
-								关系
-							</el-col>
-							<el-col :xs="6" :sm="6" :md="6" :lg="5">
-								<el-select v-model="ruleForm.applicantDegree" placeholder="请选择与联系人之间的关系">
-									<el-option v-for="item in relationList" :key="item.value" :label="item.degree" :value="item.degreeValue">
-									</el-option>
-								</el-select>
-							</el-col>
-							<el-col :xs="2" :sm="2" :md="2" :lg="1">
-								姓名
-							</el-col>
-							<el-col :xs="6" :sm="6" :md="6" :lg="5">
-								<el-input v-model="company.distance"></el-input>
-							</el-col>
-							<el-col :xs="2" :sm="2" :md="2" :lg="2">
-								手机号
-							</el-col>
-							<el-col :xs="6" :sm="6" :md="6" :lg="4">
-								<el-input v-model="company.distance"></el-input>
-							</el-col>
+						<el-form-item label="联系人信息" prop="">
+							<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-position="center" label-width="70px" class="demo-ruleForm">
+								<el-row style="margin-bottom: 10px;">
+									<el-col :xs="6" :sm="6" :md="6" :lg="6">
+										<el-form-item label="关系" prop="applicantPercent">
+											<el-select v-model="ruleForm.msgBind1" placeholder="请选择与联系人之间的关系">
+												<el-option v-for="item in relationList" :key="item.value" :label="item.relationship" :value="item.relationValue">
+												</el-option>
+											</el-select>
+										</el-form-item>
+									</el-col>
+									<el-col :xs="6" :sm="6" :md="6" :lg="6">
+										<el-form-item label="姓名" prop="applicantPercent">
+											<el-input :maxlength="30" v-model="ruleForm.msgName1"></el-input>
+										</el-form-item>
+									</el-col>
+									<el-col :xs="6" :sm="6" :md="6" :lg="6">
+										<el-form-item label="手机号" prop="applicantPercent">
+											<el-input :maxlength="11" v-model="msg.msgTel1"></el-input>
+										</el-form-item>
+									</el-col>
+								</el-row>
+								<el-row style="margin-bottom: 10px;">
+									<el-col :xs="6" :sm="6" :md="6" :lg="6">
+										<el-form-item label="关系" prop="applicantPercent">
+											<el-select v-model="ruleForm.msgBind2" placeholder="请选择与联系人之间的关系">
+												<el-option v-for="item in relationList" :key="item.value" :label="item.relationship" :value="item.relationValue">
+												</el-option>
+											</el-select>
+										</el-form-item>
+									</el-col>
+									<el-col :xs="6" :sm="6" :md="6" :lg="6">
+										<el-form-item label="姓名" prop="applicantPercent">
+											<el-input :maxlength="30" v-model="ruleForm.msgName2"></el-input>
+										</el-form-item>
+									</el-col>
+									<el-col :xs="6" :sm="6" :md="6" :lg="6">
+										<el-form-item label="手机号" prop="applicantPercent">
+											<el-input :maxlength="11" v-model="msg.msgTel2"></el-input>
+										</el-form-item>
+									</el-col>
+								</el-row>
+								<el-row style="margin-bottom: 10px;">
+									<el-col :xs="6" :sm="6" :md="6" :lg="6">
+										<el-form-item label="关系" prop="applicantPercent">
+											<el-select v-model="ruleForm.msgBind3" placeholder="请选择与联系人之间的关系">
+												<el-option v-for="item in relationList" :key="item.value" :label="item.relationship" :value="item.relationValue">
+												</el-option>
+											</el-select>
+										</el-form-item>
+									</el-col>
+									<el-col :xs="6" :sm="6" :md="6" :lg="6">
+										<el-form-item label="姓名" prop="applicantPercent">
+											<el-input :maxlength="30" v-model="ruleForm.msgName3"></el-input>
+										</el-form-item>
+									</el-col>
+									<el-col :xs="6" :sm="6" :md="6" :lg="6">
+										<el-form-item label="手机号" prop="applicantPercent">
+											<el-input :maxlength="11" v-model="msg.msgTel3"></el-input>
+										</el-form-item>
+									</el-col>
+								</el-row>
+							</el-form>
 						</el-form-item>
 					</el-col>
 				</el-row>
@@ -407,7 +448,7 @@
 					</el-col>
 					<el-col :xs="6" :sm="6" :md="6" :lg="6">
 						<el-form-item prop="name">
-							<el-input v-model="ruleForm.name" placeholder="请列举其他优势"></el-input>
+							<el-input :maxlength="200" v-model="ruleForm.name" placeholder="请列举其他优势"></el-input>
 						</el-form-item>
 					</el-col>
 				</el-row>
@@ -460,6 +501,20 @@
 					callback();
 				}
 			};
+			var checkNum9 = (rule, value, callback) => {
+				if(!/^\d{9}$/.test(value)) {
+					return callback(new Error('请输入正确的工号'));
+				} else {
+					callback();
+				}
+			};
+			var checkPercent = (rule, value, callback) => {
+				if(!/^\d\.([1-9]{1,2}|[0-9][1-9])$|^[1-9]\d{0,1}(\.\d{1,2}){0,1}$|^100(\.0{1,2}){0,1}$/.test(value)) {
+					return callback(new Error('请输入正确的百分比'));
+				} else {
+					callback();
+				}
+			};
 			return {
 				msg: 'WD1234567890',
 				ruleForm: {
@@ -486,14 +541,31 @@
 					legalTel: '', //法人电话
 					legalId: '', //法人身份证号
 					threeMoney: '', //近三月平均营业额
-					yearMoney:'',//去年全年营业额
-					productName1:'',//商品名称1
-					productName2:'',//商品名称2
-					productName3:'',//商品名称3
-					productPrice1:'',//商品价格1
-					productPrice2:'',//商品价格2
-					productPrice3:'',//商品价格3
+					yearMoney: '', //去年全年营业额
+					productName1: '', //商品名称1
+					productName2: '', //商品名称2
+					productName3: '', //商品名称3
+					productPrice1: '', //商品价格1
+					productPrice2: '', //商品价格2
+					productPrice3: '', //商品价格3
+					dailyPeople: '', //店铺日均人流量
+					applicantName: '', //申请人姓名
+					healthStatus: '', //健康状况
 					applicantDegree: '', //申请人学历
+					maritalStatus: '', //婚姻状况
+					applicantOrigin: '', //申请人籍贯
+					applicantResAddress: '', //申请人户籍地址
+					applicantCurrAddress: '', //申请人现居住地址
+					applicantPercent: '', //申请人占股比列
+					msgBind1: '',//联系人信息
+					msgBind2: '',
+					msgBind3: '',
+					msgName1: '',
+					msgName2: '',
+					msgName3: '',
+					msgTel1: '',
+					msgTel2: '',
+					msgTel3: '',
 					value1: '',
 					type: ''
 				},
@@ -531,18 +603,18 @@
 					}
 				],
 				recommended: [{
-						channelName: '外部渠道',
-						applicationNo: '1'
-					},
-					{
-						channelName: '内部渠道',
-						applicationNo: '0'
-					},
-					{
-						channelName: '无',
-						applicationNo: '-1'
-					}
-				], //推荐渠道
+					channel: "全部",
+					value: "0"
+				}, {
+					channel: "外部渠道",
+					value: "1"
+				}, {
+					channel: "内部渠道",
+					value: "2"
+				}, {
+					channel: "无",
+					value: "3"
+				}], //推荐渠道
 				channels: [{
 						channelName: '外部渠道',
 						applicationNo: '1'
@@ -609,70 +681,70 @@
 					}
 
 				],
-				healthList: [{ //健康状况
+				healthList: [{
 						healthStatus: "健康",
-						healthValue: "2"
+						healthValue: "1"
 					},
 					{
 						healthStatus: "一般",
-						healthValue: "3"
+						healthValue: "2"
 					},
 					{
 						healthStatus: "较差",
-						healthValue: "4"
+						healthValue: "3"
 					},
 					{
 						healthStatus: "有疾病",
-						healthValue: "5"
+						healthValue: "4"
 					}
 				],
-				degrees: [{ //学历
+				degrees: [{
 						degree: "小学",
-						degreeValue: "0"
-					},
-					{
-						degree: "初中",
 						degreeValue: "1"
 					},
 					{
-						degree: "高中",
+						degree: "初中",
 						degreeValue: "2"
-					}, {
-						degree: "大专",
+					},
+					{
+						degree: "高中",
 						degreeValue: "3"
 					}, {
-						degree: "本科",
+						degree: "大专",
 						degreeValue: "4"
 					}, {
-						degree: "硕士研究生",
+						degree: "本科",
 						degreeValue: "5"
 					}, {
-						degree: "博士研究生",
+						degree: "硕士研究生",
 						degreeValue: "6"
+					}, {
+						degree: "博士研究生",
+						degreeValue: "7"
 					}
 				],
-				maritalList: [{ //婚姻状况
+				maritalList: [{
 					maritalStatus: "未婚",
-					maritalValue: "0"
-				}, {
-					maritalStatus: "已婚",
 					maritalValue: "1"
 				}, {
-					maritalStatus: "离异",
+					maritalStatus: "已婚",
 					maritalValue: "2"
 				}, {
-					maritalStatus: "丧偶",
+					maritalStatus: "离异",
 					maritalValue: "3"
+				}, {
+					maritalStatus: "丧偶",
+					maritalValue: "4"
 				}],
 				relationList: [{
 					relationship: "配偶",
-					relationValue: "0"
-				}, {
-					relationship: "直系亲属",
 					relationValue: "1"
 				}, {
-					relationship: "同事",
+					relationship: "直系亲属",
 					relationValue: "2"
+				}, {
+					relationship: "同事",
+					relationValue: "3"
 				}],
 				pickerOptions0: {
 					disabledDate(time) {
@@ -739,6 +811,9 @@
 					salesmanNumber: [{
 						required: true,
 						message: '请输入业务员工号',
+						trigger: 'blur'
+					}, {
+						validator: checkNum9,
 						trigger: 'blur'
 					}],
 					companyName: [{
@@ -844,6 +919,84 @@
 					}, {
 						validator: checkNum,
 						trigger: 'blur'
+					}],
+					dailyPeople: [{
+						required: true,
+						message: '请输入店铺日均人流量',
+						trigger: 'blur'
+					}],
+					applicantName: [{
+						required: true,
+						message: '请输入申请人姓名',
+						trigger: 'blur'
+					}],
+					healthStatus: [{
+						required: true,
+						message: '请选择健康状况',
+						trigger: 'change'
+					}],
+					applicantDegree: [{
+						required: true,
+						message: '请选择申请人学历',
+						trigger: 'change'
+					}],
+					maritalStatus: [{
+						required: true,
+						message: '请选择婚姻状况',
+						trigger: 'change'
+					}],
+					applicantOrigin: [{
+						required: true,
+						message: '请输入申请人籍贯',
+						trigger: 'blur'
+					}],
+					applicantResAddress: [{
+						required: true,
+						message: '请输入申请人户籍地址',
+						trigger: 'blur'
+					}],
+					applicantCurrAddress: [{
+						required: true,
+						message: '请输入申请人现居住地址',
+						trigger: 'blur'
+					}],
+					applicantPercent: [{
+						required: true,
+						message: '请输入申请人占股比列',
+						trigger: 'blur'
+					}, {
+						validator: checkPercent,
+						trigger: 'blur'
+					}],
+					msgBind1:[{
+						required: true,
+						message: '请选择与联系人关系',
+						trigger: 'change'
+					}],
+					msgBind2:[{
+						required: true,
+						message: '请选择与联系人关系',
+						trigger: 'change'
+					}],
+					msgBind3:[{
+						required: true,
+						message: '请选择与联系人关系',
+						trigger: 'change'
+					}],
+					msgName1:[{
+						required: true,
+						message: '请选择与联系人关系',
+						trigger: 'change'
+					}],
+					msgName2:[{
+						required: true,
+						message: '请选择与联系人关系',
+						trigger: 'change'
+					}],
+					msgName3:[{
+						required: true,
+						message: '请选择与联系人关系',
+						trigger: 'change'
 					}],
 				}
 			}
