41aa4c3e7ca07c0d170b8095fbb42c1bd99b7f3f
hsp130208358005 <13020835005@163.com>
2017-08-03 11:44:10 +0800

    login 优化

--- src/components/login.vue
+++ src/components/login.vue
@@ -93,6 +93,10 @@
 					return false;
 				}else{
 				var countdown = 60;
+				this.getCodeTime++;
+				if(this.getCodeTime>1){
+					this.this.getCodeTime=2;
+				}
 				this.$http.post(process.env.API + "/getMessageCode", {
 					"userId": this.ruleForm.username,
 					"getMessageCodeFlag":this.getCodeTime
